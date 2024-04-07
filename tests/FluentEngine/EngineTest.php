8402cfc4b2e67c9db59b27599d295e1c749951e6
nettools.ovh <dev@nettools.ovh>
2024-04-07 10:03:42 +0200

    fix test

--- tests/FluentEngine/EngineTest.php
+++ tests/FluentEngine/EngineTest.php
@@ -5,6 +5,7 @@ namespace Nettools\Mailing\FluentEngine\Tests;
 
 use \Nettools\Mailing\MailSenders\Virtual;
 use \Nettools\Mailing\FluentEngine\ComposeEngine;
+use \Nettools\Mailing\MailBuilder\MixedRelated;
 use \Nettools\Mailing\MailBuilder\Builder;
 use \Nettools\Mailing\Mailer;
 use \org\bovigo\vfs\vfsStream;
@@ -295,7 +296,7 @@ class EngineTest extends \PHPUnit\Framework\TestCase
       
     public function testAttachmentsCache()
     {
-		$cache = Builder::getAttachmentsCache();
+		$cache = MixedRelated::getAttachmentsCache();
 		$cache->clear();
 		$c0 = $cache->getCount();
 
